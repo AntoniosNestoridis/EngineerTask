8c380ff6ae98115b1b777c33ab397f15890c1559
Antonios Nestoridis <nestoridis.antonai@gmail.com>
2025-11-13 13:18:31 +0100

    Restructured slightly parsing process. Brought Regex example for splitting string

--- Assets/01 Scripts/Parser/IXMLFileParser.cs
+++ Assets/01 Scripts/Parser/IXMLFileParser.cs
@@ -1,18 +1,20 @@
 using UnityEngine;
 using System.IO;
 using UnityEditor;
+using System;
+using System.Text.RegularExpressions;
 
 public class IXMLFileParser : MonoBehaviour
 {
 
-    public string FileDirectory;
+    public string FileDirectory; // TODO: Eventually we want this to be the Application.Filepath
     public string filename;
 
     public bool ReadFile;
 
     void Start()
     {
-        Debug.Log("Hello");
+       
     }
 
 
@@ -21,34 +23,119 @@ public class IXMLFileParser : MonoBehaviour
         if (ReadFile)
         {
             ReadFile = false;
-            ReadFileFromDisk(filename);
+            CreateBrickData();
         }
     }
+
+
+
+    public void CreateBrickData()
+    {
+        // 1. Read text file from disk
+        string fileContents = ReadFileFromDisk(FileDirectory,filename);
+
+        // 2. Parse file into a data form
+        if (!string.IsNullOrEmpty(fileContents))
+        {
+            ParseFile(fileContents);
+        }
+
+        // 3. Display results
+    }
+
 
     /// <summary>
     /// Parses an IXFML file and creates a new instance of brick data information
     /// </summary>
-    public void ParseFile()
+    public void ParseFile(string fileContents)
     {
-            
+        // We assume a valid string here
+
+        Debug.Log("Attempting to Parse text file into data...");
+
+        string text = "One car red car blue car";
+        string pat = @"(\w+)\s+(car)";
+
+        // Instantiate the regular expression object.
+        Regex r = new Regex(pat, RegexOptions.IgnoreCase);
+
+        // Match the regular expression pattern against a text string.
+        var splitList = r.Split(text);
+        int matchCount = splitList.Length;
+
+        Debug.Log($"Found {matchCount} entries");
+
+        foreach (var split in splitList)
+        {
+            Debug.Log(split);
+        }
+
+        //while (m.Success)
+        //{
+        //    Console.WriteLine("Match"+ (++matchCount));
+        //    for (int i = 1; i <= 2; i++)
+        //    {
+        //    Group g = m.Groups[i];
+        //    Console.WriteLine("Group"+i+"='" + g + "'");
+        //    CaptureCollection cc = g.Captures;
+        //    for (int j = 0; j < cc.Count; j++)
+        //    {
+        //        Capture c = cc[j];
+        //        System.Console.WriteLine("Capture"+j+"='" + c + "', Position="+c.Index);
+        //    }
+        //    }
+        //    m = m.NextMatch();
+        //}
+
+
+        // 1. List of individual bricks
+
+        // 2. Parse brick data
+
+        // 3. List of parts 
+
+        // 4. Parse part data
+
+        // 5. 
+
+
     }
 
     /// <summary>
-    /// 
+    /// Reads a text file from the disk and returns its contents as a string or an empty string if there is an error in the process.
     /// </summary>
-    public void ReadFileFromDisk(string filename)
+    /// <param name="directory">The directory path </param>
+    /// <param name="filename"> Name of the file to read </param>
+    /// <returns></returns>
+    public string ReadFileFromDisk(string directory, string filename)
     {
 
-        //AssetDatabase.ImportAsset(path); 
-        //Application.dataPath
-        // Example path Assets/04 Data/everyoneisawesome
-        var sr = new StreamReader( FileDirectory + "/" + filename);
-        var fileContents = sr.ReadToEnd();
-        sr.Close();
-     
+        string fileContents = "";
 
-        //Print the text from the file
-        Debug.Log(fileContents);
+        Debug.Log("Attempting to read file contents...");
+
+        try
+        {
+            // TODO: Check if directory and/or filename exist first before attempting to read them. But I guess the try catch does the same job.
+            // Create an instance of StreamReader to read from a file.
+            using (StreamReader sr = new StreamReader( directory + "/" + filename))
+            {
+                fileContents = sr.ReadToEnd();
+                
+                if (!string.IsNullOrEmpty(fileContents))
+                {
+                    Debug.Log("Success!");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            // Error output
+            Debug.LogError("The file could not be read:");
+            Debug.LogError(e.Message);
+        }
+
+        return fileContents;     
     }
 
     
