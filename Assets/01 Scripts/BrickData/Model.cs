ab92fcf274c99705d154d0343cf1d9fd32650ee4
Antonios Nestoridis <nestoridis.antonai@gmail.com>
2025-11-13 16:43:17 +0100

    Forgot model quick print function

--- Assets/01 Scripts/BrickData/Model.cs
+++ Assets/01 Scripts/BrickData/Model.cs
@@ -10,6 +10,30 @@ public class Model : MonoBehaviour
 {
     public int TotalBricks;
 
-    public List<Brick> bricks; // A list of the bricks that this model contains
+    public List<Brick> bricks = new List<Brick>(); // A list of the bricks that this model contains
+
+    public void PrintModelDataToConsole()
+    {
+
+        Debug.Log("--- Model information --- ");
+
+        foreach (var brick in bricks)
+        {
+            Debug.Log($"Brick-UUID = {brick.uuid}");
+            Debug.Log($"Brick-DesignID = {brick.designID}");
+
+            Debug.Log($"Part-UUID = {brick.partuuID}");
+            Debug.Log($"Part-DesignID = {brick.partDesignID}");
+            Debug.Log($"Part-Type = {brick.partType}");
+            Debug.Log($"Part-Materials = {brick.partMaterials}");
+
+            Debug.Log($"Bone-UUID = {brick.uuid}");
+            Debug.Log($"Bone-Transformation = {brick.boneTransformation}");
+
+        }
+
+        Debug.Log("--- --- --- --- ");
+    }
+
 
 }
